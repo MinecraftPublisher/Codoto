b23d4ec0c977b9496036f25d0b9038b1d8195070
martia_f <mforoud86@gmail.com>
2021-09-28 09:23:02 +0330

    Added `compiler.js`.

---
+++ bin/compiler.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+/*
+ * Codoto compiler
+ * Codoto compiler is a part of codoto that compiles codoto code to Javascript.
+ */
---
+++ bin/index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+/*
+ * Codoto
+ * Write entire functions just by entering a few digits.
+ * Codoto is a Javascript framework that lets users compile functions and files onto digits, And submit them to the main server.
+ * Then, Other users could use that database entry to compile it to a function.
+ */
